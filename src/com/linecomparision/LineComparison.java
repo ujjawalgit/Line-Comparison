5b2ecfe48bc5fca2ce0f2f301b46cd13005c01be
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-06-24 19:39:08 +0530

    added: Welcome message

---
+++ src/com/linecomparision/LineComparison.java
@@ -0,0 +1,9 @@
+package com.linecomparision;
+
+public class LineComparison {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+
+    }
+}
