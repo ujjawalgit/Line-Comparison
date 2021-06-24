3d59b31badcd23b8743195fd994e3ea83f395029
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-06-24 20:06:41 +0530

    added: compare two lines based on the end points using OOP

--- src/com/linecomparision/LineComparison.java
+++ src/com/linecomparision/LineComparison.java
@@ -3,10 +3,34 @@ package com.linecomparision;
 import java.util.Scanner;
 
 public class LineComparison {
+    public static Double x1, y1;// start point is same for each line
+    public Double x, y;//end point
+    public Double dis;
+// calculation of length between two points
+    public void getDistance() {
+        this.dis = Math.sqrt((x1 - this.x) * (x1 - this.x) + (y1 - this.y) * (y1 - this.y));
+    }
+// comparing two lines
+    public static void compareLine(LineComparison line1, LineComparison line2) {
+        System.out.println("line1 : distance between" + "(" + x1 + "," + y1 + ")," + "(" + line1.x + "," + line1.y + ")=" + line1.dis);
+        System.out.println("line2 : distance between" + "(" + x1 + "," + y1 + ")," + "(" + line2.x + "," + line2.y + ")=" + line2.dis);
+
+        if (line1.dis.equals(line2.dis)) {
+            System.out.println("length of line1 and line2 are equal");
+        } else {
+            if (line1.dis > line2.dis) {
+                System.out.println("length of line1 is greater than line2");
+            } else {
+                System.out.println("length of line1 is lesser than line2");
+            }
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
-        Double x1, x2, x3, y1, y2, y3, dis1, dis2;
+
+        LineComparison line1 = new LineComparison();
+        LineComparison line2 = new LineComparison();
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter coordinate of initial point(x1,y1):");
@@ -14,28 +38,15 @@ public class LineComparison {
         y1 = scanner.nextDouble();
 
         System.out.println("enter coordinate of end point of line1(x2,y2):");
-        x2 = scanner.nextDouble();
-        y2 = scanner.nextDouble();
-
+        line1.x = scanner.nextDouble();
+        line1.y = scanner.nextDouble();
+        line1.getDistance();
         System.out.println("enter coordinate of end point of line2(x3,y3):");
-        x3 = scanner.nextDouble();
-        y3 = scanner.nextDouble();
+        line2.x = scanner.nextDouble();
+        line2.y = scanner.nextDouble();
+        line2.getDistance();
+
+        compareLine(line1,line2);
         scanner.close();
-
-        dis1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        dis2 = Math.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
-
-        System.out.println("distance between" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")=" + dis1);
-        System.out.println("distance between" + "(" + x1 + "," + y1 + ")," + "(" + x3 + "," + y3 + ")=" + dis2);
-
-        if (dis1.equals(dis2)) {
-            System.out.println("length of line1 and line2 are equal");
-        } else {
-            if (dis1 > dis2) {
-                System.out.println("length of line1 is greater than line2");
-            } else {
-                System.out.println("length of line1 is lesser than line2");
-            }
-        }
     }
 }
